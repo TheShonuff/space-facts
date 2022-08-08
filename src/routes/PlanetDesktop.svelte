e7ae652f4156d357ae8ae9c6f656980589659767
Joe Irvine <jirvine124@gmail.com>
2022-08-07 23:22:57 -0700

    added ability for planets to change sizes visually based on a selection

--- src/routes/PlanetDesktop.svelte
+++ src/routes/PlanetDesktop.svelte
@@ -22,6 +22,16 @@
 		'#2D68F0'
 	];
 	console.log('Desktop Loaded');
+	const imgSizes = [
+		{ Name: 'Mercury', Desktop: 290, Tablet: 184, Mobile: 111 },
+		{ Name: 'Venus', Desktop: 400, Tablet: 253, Mobile: 154 },
+		{ Name: 'Earth', Desktop: 450, Tablet: 285, Mobile: 174 },
+		{ Name: 'Mars', Desktop: 336, Tablet: 213, Mobile: 129 },
+		{ Name: 'Jupiter', Desktop: 582, Tablet: 369, Mobile: 224 },
+		{ Name: 'Saturn', Desktop: 668, Tablet: 422, Mobile: 256 },
+		{ Name: 'Uranus', Desktop: 458, Tablet: 290, Mobile: 176 },
+		{ Name: 'Neptune', Desktop: 450, Tablet: 285, Mobile: 173 }
+	];
 </script>
 
 <svelte:head>
@@ -30,7 +40,12 @@
 
 <div class="container">
 	<div class="main-content">
-		<div class="planet-img">
+		<div
+			class="planet-img"
+			style={$screenwidth > 900
+				? '--planet-size:{imgSizes[selection].Desktop}'
+				: '--planet-size:{imgSizes[selection].Tablet}'}
+		>
 			{#if infoDisplay === 1}
 				<img src={`${Data[selection].images.planet}`} alt="planet" />
 			{:else if infoDisplay === 2}
